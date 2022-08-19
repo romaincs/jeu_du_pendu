8408c6e66897bfed9a7909c39e332420734497d3
Romain Cressens <romaincs@live.fr>
2022-08-19 16:29:27 +0200

    feat(chore): first simple version of the game

---
+++ Pendu.cs
@@ -0,0 +1,139 @@
+﻿using System;
+namespace jeu_du_pendu
+{
+    public class Pendu
+    {
+        private static string[] _PENDU_ASCII = { @"
+                  +---+
+                  |   |
+                      |
+                      |
+                      |
+                      |
+                =========",
+
+                @"
+                  +---+
+                  |   |
+                  O   |
+                      |
+                      |
+                      |
+                =========",
+
+                @"
+                  +---+
+                  |   |
+                  O   |
+                  |   |
+                      |
+                      |
+                =========
+                ",
+                @"
+                  +---+
+                  |   |
+                  O   |
+                 /|   |
+                      |
+                      |
+                =========
+                ",
+                @"
+                  +---+
+                  |   |
+                  O   |
+                 /|\  |
+                      |
+                      |
+                =========
+                ",
+                @"
+                  +---+
+                  |   |
+                  O   |
+                 /|\  |
+                 /    |
+                      |
+                =========
+                ",
+                @"
+                  +---+
+                  |   |
+                  O   |
+                 /|\  |
+                 / \  |
+                      |
+                =========
+                "
+            };
+        private string _mot;
+        private List<char> _lettresTrouves = new List<char>();
+        private int _nbEssai;
+
+        public string Mot
+        {
+            get
+            {
+                return _mot;
+            }
+        }
+
+        public bool Gagne
+        {
+            get
+            {
+                return _lettresTrouves.Count == _mot.Distinct().Count();
+            }
+        }
+
+        public bool Perdu
+        {
+            get
+            {
+                return _nbEssai == _PENDU_ASCII.Length-1;
+            }
+        }
+
+        public Pendu(string mot)
+        {
+            _mot = mot.ToLower();
+        }
+
+        public Pendu(List<string> mots)
+        {
+            Random r = new Random();
+            int indexMot = r.Next(0, mots.Count());
+            _mot = mots[indexMot].ToLower();
+        }
+
+        public string RecupererAscii()
+        {
+            return _PENDU_ASCII[_nbEssai];
+        }
+
+        public string RecupererLettresTrouves()
+        {
+            string lettres = "";
+
+            foreach (var lettre in _mot)
+            {
+                if (_lettresTrouves.Contains(lettre))
+                    lettres += lettre + " ";
+                else
+                    lettres += " _ ";
+            }
+
+            return lettres.ToUpper();
+        }
+
+        public void NouvelEssai(string lettre)
+        {
+            if (_mot.Contains(lettre.ToLower()))
+                _lettresTrouves.Add(lettre[0]);
+            else
+                _nbEssai++;
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,3 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace jeu_du_pendu
+{
+    public class Program
+    {
+        public const string FILE_PATH = @"/Users/romaincs/Workspace/jeu_du_pendu/liste_mots.txt";
+
+        public static void Main(string[] args)
+        {
+            string? recommencer = "o";
+            while (recommencer == "o")
+            {
+                List<string> mots = RecupererListeMots(FILE_PATH);
+                Pendu pendu = new(mots);
+
+                while (true)
+                {
+                    Console.Clear();
+                    string dessin = pendu.RecupererAscii();
+                    Console.WriteLine(dessin + "\n\n");
+
+                    if (pendu.Gagne)
+                    {
+                        Console.WriteLine("Bravo ! Vous avez trouvé le mot.");
+                        break;
+                    }
+                    if (pendu.Perdu)
+                    {
+                        Console.WriteLine("Désolé, vous avez perdu :-(");
+                        Console.WriteLine("Le mot a trouvé était: " + pendu.Mot);
+                        break;
+                    }
+
+                    string lettresTrouves = pendu.RecupererLettresTrouves();
+                    Console.WriteLine(lettresTrouves + "\n\n");
+
+                    Console.Write("Saisissez une lettre: ");
+                    string? lettre = Console.ReadLine();
+                    if (string.IsNullOrEmpty(lettre) || lettre.Length != 1)
+                        Console.WriteLine("Vous devez saisir une lettre");
+                    else
+                        pendu.NouvelEssai(lettre);
+                }
+
+                Console.Write("Nouvelle parties (o/n) ? ");
+                recommencer = Console.ReadLine();
+
+            }            
+        }
+
+        public static List<string> RecupererListeMots(string filePath)
+        {
+            string[] lines = File.ReadAllLines(filePath);
+            return lines.ToList<string>();
+            
+        }
+    }
+}
